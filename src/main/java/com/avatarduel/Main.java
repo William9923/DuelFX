f0309e73384a60bc185deb8fc14f0fa5842d7abc
William Ong <williamong1400@noreply.com>
2020-03-31 00:24:50 +0700

    Revert "loader class finish"
    This reverts commit e53fc545680952ca3a0ef185a074c0f42468cb0b.

--- src/main/java/com/avatarduel/Main.java
+++ src/main/java/com/avatarduel/Main.java
@@ -1,16 +1,74 @@
 package com.avatarduel;
 
-import java.util.*;
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.List;
+
+import com.avatarduel.model.Element;
+import com.avatarduel.model.Land;
+import com.avatarduel.util.CSVReader;
 
 public class Main {
 
+    private static final String LAND_CSV_FILE_PATH = "card/data/land.csv";
+    private static final String CHARACTER_CSV_FILE_PATH = "card/data/character.csv";
+    private static final String SKILL_CSV_FILE_PATH = "card/data/skill_aura.csv";
+
+
+    public void loadCards(String filename) throws IOException, URISyntaxException {
+        File landCSVFile = new File(getClass().getResource(filename).toURI());
+        CSVReader landReader = new CSVReader(landCSVFile, "\t");
+        landReader.setSkipHeader(true);
+        List<String[]> landRows = landReader.read();
+        for (String[] row : landRows) {
+            testPrintString(row);
+            Land l = new Land(row[1], row[3], Element.valueOf(row[2]));
+        }
+    }
+
+    public void loadLand() {
+        try{
+            loadCards(LAND_CSV_FILE_PATH);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void loadCharacter() {
+        try{
+            loadCards(CHARACTER_CSV_FILE_PATH);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void loadSkill() {
+        try{
+            loadCards(SKILL_CSV_FILE_PATH);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void testPrintString(String[] arr) {
+        for (String text : arr) {
+            System.out.print(text + " ");
+        }
+        System.out.println();
+    }
     public static void main(String[] args) {
-        ArrayList<String[]> arr = new ArrayList<String[]>();
-        String[] a = new String[] {"a", "b", "c"};
-        String[] b = new String[] {"a", "b", "c"};
-        arr.add(a);
-        arr.add(b);
-        System.out.println(arr.get(0));
         System.out.println("Hello World");
+        Main main = new Main();
+        main.loadCharacter();
+        main.loadLand();
+        main.loadSkill();
+
     }
 }
