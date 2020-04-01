829d44686e2f47b54bf388cdafedf4d5d3087bcd
Arthur Edgar Y <arthuredgary24@gmail.com>
2020-04-01 22:30:02 +0700

    Initial Player

---
+++ src/main/java/com/avatarduel/model/Player.java
@@ -0,0 +1,15 @@
+package com.avatarduel.model;
+
+import com.avatarduel.type.PlayerType;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class Player extends {
+    protected Deck deck;
+    protected Field field;
+    protected Hand hand;
+    protected PlayerType A, B;
+    protected PowerManager power;
+}
