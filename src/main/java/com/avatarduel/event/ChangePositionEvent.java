e981ee544625efe08a899f4d651b439bb8351bbb
William9923 <13518138@std.stei.itb.ac.id>
2020-04-19 09:05:34 +0700

    Change Position added javadoc

--- src/main/java/com/avatarduel/event/ChangePositionEvent.java
+++ src/main/java/com/avatarduel/event/ChangePositionEvent.java
@@ -8,7 +8,21 @@ import com.avatarduel.model.card.CharacterCardInField;
 import com.avatarduel.model.type.Phase;
 import com.avatarduel.model.type.PlayerType;
 
-
+/**
+ * ChangePositionEvent is a event for changing current player position (ATTACK / DEFENSE).
+ *
+ * If the character targeted is in Attack Position, it will be switched to the Defense Position, vice versa
+ * NOTE :
+ * If the character being targeted by attack event in Attack Position, then the stats that counted were the Atk
+ * While if the character being targeted by attack event in Defense Position, then the stats that counted were the Def  
+ *
+ * IMPORTANT NOTE:
+ * This event will communicate with game singleton instantly, so there are no need to validate
+ * In case where event is not possible to do, we throw exception so that the GUI Board can give the
+ * error message to the player playing the games
+ *
+ * @author G10-K03-CardGameOOP
+ */
 
 public class ChangePositionEvent implements IEvent {
 
