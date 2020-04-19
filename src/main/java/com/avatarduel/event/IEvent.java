3e24dced8d62532e973bd70dc6ebd87fc84e4375
Arthur Edgar Y <arthuredgary24@gmail.com>
2020-04-19 18:56:41 +0700

    Tambahin javadoc

--- src/main/java/com/avatarduel/event/IEvent.java
+++ src/main/java/com/avatarduel/event/IEvent.java
@@ -2,6 +2,15 @@ package com.avatarduel.event;
 
 import com.avatarduel.exception.InvalidOperationException;
 
+/**
+ * IEvent is an interface which use to various event classes in event package.
+ *
+ * IMPORTANT NOTE:
+ * IEvent only has one method "execute" that will throw InvalidOperationException
+ * if and only if the method in the classes doesn't fullify the spesification.
+ * @author G10-K03-CardGameOOP
+ */
+
 public interface IEvent {
     public void execute() throws InvalidOperationException;
 }
--- src/main/java/com/avatarduel/exception/ExceptionCause/ExceptionCause.java
+++ src/main/java/com/avatarduel/exception/ExceptionCause/ExceptionCause.java
@@ -1,5 +1,11 @@
 package com.avatarduel.exception.ExceptionCause;
 
+/**
+ * ExceptionCause is an interface to use in varies exception that usually
+ * other classes will overide due to many causes of the exception.
+ * @author G10-K03-CardGameOOP
+ */
+
 public interface ExceptionCause {
     public String getCause();
 }
