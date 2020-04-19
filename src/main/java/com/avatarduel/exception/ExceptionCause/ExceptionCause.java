4dc4dcabb48c0d369d13014d9869b0eb8a0f0779
margunwa123 <13518114@std.stei.itb.ac.id>
2020-04-19 13:24:11 +0700

    bersihin javadoc dikit

--- src/main/java/com/avatarduel/exception/ExceptionCause/ExceptionCause.java
+++ src/main/java/com/avatarduel/exception/ExceptionCause/ExceptionCause.java
@@ -2,6 +2,9 @@ package com.avatarduel.exception.ExceptionCause;
 
 /**
  * Defines the cause of an exception
+ * The difference between Exception and cause is,
+ * the exception defines what the player cannot do
+ * The cause describe why the player cannot do it
  */
 
 public interface ExceptionCause {
