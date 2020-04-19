774a7f8ec090c14522ab233a38aaa71cbcf9ab02
William9923 <13518138@std.stei.itb.ac.id>
2020-04-19 23:55:32 +0700

    Add Phase Documentation

--- src/main/java/com/avatarduel/phase/EndPhase.java
+++ src/main/java/com/avatarduel/phase/EndPhase.java
@@ -2,22 +2,38 @@ package com.avatarduel.phase;
 
 import com.avatarduel.model.type.Phase;
 
+/**
+ * EndPhase is a representation of end phase in the game.
+ * IMPORTANT NOTE:
+ * Player are not going to play in this phase.
+ * This phase only a representation to change the game state into other player
+ * @author G10-K03-CardGameOOP
+ */
+
 public class EndPhase implements IPhase {
 
     private Phase phase;
 
-    //Constructor for EndPhase
+    /**
+     * EndPhase Constructor
+     */
     public EndPhase() {
         phase = Phase.END;
     }
 
-    //Method to return current Phase
+    /**
+     * Get the Phase Enum
+     * @return Phase Object Representation
+     */
     @Override
     public Phase getPhase() {
         return phase;
     }
 
-    //Method to get to the next phase
+    /**
+     * Change the phase into next phase using state design pattern
+     * @return IPhase -> representation of next phase
+     */
     @Override
     public IPhase next() {
         return new DrawPhase();
--- src/main/java/com/avatarduel/phase/IPhase.java
+++ src/main/java/com/avatarduel/phase/IPhase.java
@@ -2,11 +2,15 @@ package com.avatarduel.phase;
 
 import com.avatarduel.model.type.Phase;
 
+/**
+ * Interface for Phase Object in Game
+ * Contains 2 method :
+ * 1. getPhase
+ * 2. next
+ *
+ * @author  G10-K03-CardGameOOP
+ */
 public interface IPhase {
-
     public Phase getPhase();
-    //Interface for antoher phases class
-
     public IPhase next();
-    //Interface for another phases class
 }
