ee213eb8d30d202b51ea515cbea760f7d6550353
William9923 <13518138@std.stei.itb.ac.id>
2020-04-19 10:55:12 +0700

    add Deck Test

---
+++ src/test/java/com/avatarduel/model/player_component/DeckTest.java
@@ -0,0 +1,5 @@
+package com.avatarduel.model.player_component;
+
+class DeckTest {
+
+}
---
+++ src/test/java/com/avatarduel/model/player_component/DeckTestTest.java
@@ -0,0 +1,7 @@
+package com.avatarduel.model.player_component;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DeckTestTest {
+
+}
